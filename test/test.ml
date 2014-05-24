92adf42bf8f6bf95ace5117f88f3e4bc4da84c3a
Blue Prawn <monnier.florent@gmail.com>
2014-05-24 15:01:34 +0159

    renamed the test functions

--- test/test.ml
+++ test/test.ml
@@ -9,7 +9,7 @@ let handle_return = function
   | Ptr _    -> Printf.printf "ocaml: got (Ptr)\n%!"
   | Void     -> Printf.printf "ocaml: got (Void)\n%!"
 
-let main1 () =
+let test1 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_static_handle" in
   handle_return(fficall ~func ~args:[| Float 1.1 |] ~return:Return_int);
@@ -18,35 +18,35 @@ let main1 () =
   dlclose ~lib;
 ;;
 
-let main2 () =
+let test2 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_string_param" in
   handle_return(fficall ~func ~args:[| String "one" |] ~return:Return_string);
   dlclose ~lib;
 ;;
 
-let main3 () =
+let test3 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_int_param" in
   handle_return(fficall ~func ~args:[| Int 244 |] ~return:Return_int);
   dlclose ~lib;
 ;;
 
-let main4 () =
+let test4 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_double_param" in
   handle_return(fficall ~func ~args:[| Double 211.6 |] ~return:Return_double);
   dlclose ~lib;
 ;;
 
-let main5 () =
+let test5 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_float_param" in
   handle_return(fficall ~func ~args:[| Float 211.7 |] ~return:Return_float);
   dlclose ~lib;
 ;;
 
-let main6 () =
+let test6 () =
   let lib = dlopen "./testlib.so" [RTLD_LAZY] in
   let func = dlsym ~lib ~func_name:"func_void_param" in
   handle_return(fficall ~func ~args:[| |] ~return:Return_void);
@@ -54,11 +54,11 @@ let main6 () =
 ;;
 
 let () =
-  print_newline ();  main1 ();
-  print_newline ();  main2 ();
-  print_newline ();  main3 ();
-  print_newline ();  main4 ();
-  print_newline ();  main5 ();
-  print_newline ();  main6 ();
+  print_newline ();  test1 ();
+  print_newline ();  test2 ();
+  print_newline ();  test3 ();
+  print_newline ();  test4 ();
+  print_newline ();  test5 ();
+  print_newline ();  test6 ();
 ;;
 
